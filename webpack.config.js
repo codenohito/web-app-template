de7b3cf6259674f5b19a912a386af0d4145811b8
Ivan Dymkov <mstrdymio@gmail.com>
2023-10-06 18:39:59 +0200

    Use one webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path');
+const { merge } = require('webpack-merge');
 const CopyPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const devMode = process.env.NODE_ENV !== "production";
 
-module.exports = {
+const common = {
   entry: './src/index.js',
+  output: {
+    filename: (devMode ? "bundle.js" : "bundle.[contenthash].js"),
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
   plugins: [
     new CopyPlugin({
       patterns: [
@@ -17,7 +24,10 @@ module.exports = {
         },
       ],
     }),
-    new HtmlWebpackPlugin({ template: 'public/index.html' })
+    new HtmlWebpackPlugin({ template: 'public/index.html' }),
+    new MiniCssExtractPlugin({
+      filename: (devMode ? "[name].css" : "[name].[contenthash].css"),
+    })
   ],
   module: {
     rules: [
@@ -39,3 +49,22 @@ module.exports = {
     ],
   },
 };
+
+module.exports = devMode
+  ? merge(common, {
+     mode: 'development',
+     output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
+    devtool: 'eval',
+  })
+  : merge(common, {
+    mode: 'production',
+    output: {
+      filename: 'bundle.[contenthash].js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    }
+  });
